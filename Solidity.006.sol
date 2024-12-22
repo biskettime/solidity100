ab4b27143df3b82d0f46607925487c7e9108c7ad
biskettime <biskettime>
2024-12-22 20:38:09 +0900

    06_f

--- Solidity.006.sol
+++ Solidity.006.sol
@@ -1,2 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
+
+contract ArrayPush{
+    uint[] numbers;
+
+    function fillarray() public {
+    for (uint i=0; i<=9; i++){
+        numbers.push(i);
+    }
+    }
+
+    function getNumbers() public view returns(uint[] memory){
+        return numbers;
+    }
+}
