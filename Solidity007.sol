0ee991b19e85372416484d502e403c8db65f0d7b
biskettime <biskettime>
2024-12-22 21:17:54 +0900

    07_f

--- Solidity007.sol
+++ Solidity007.sol
@@ -1,2 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
+
+contract ArrayPush{
+    uint[] numbers;
+
+    function fillarray() public {
+    for (uint i=0; i<=5; i++){
+        numbers.push(i);
+    }
+    }
+    
+    function sumup() public view returns(uint){
+        uint sumResult = 0; 
+        for (uint i = 0; i < numbers.length; i++){
+            sumResult += numbers[i];
+        }
+        return sumResult;
+    }
+}
